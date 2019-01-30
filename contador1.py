f3cdb781bd81be577dc7c3f3ce8c7b095d72195c
ninhalp <anapereira.ufrj@hotmail.com>
2019-01-30 08:58:05 -0200

    contador da aula

---
+++ contador1.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+lista = json.loads(requests.get("https://bit.ly/2RXuXsD").text)
+print(lista)
+
+#conj = set(lista)
+dicionario = {}
+#dicionario[x] = dict(lista)
+
+for x in lista:
+  if x in dicionario:
+    dicionario[x] += 1
+  else:
+    dicionario[x] = 1
+
+print(dicionario)
+print(dicionario.items)
