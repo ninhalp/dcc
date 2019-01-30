6ab066592956bf303163791531fa8a8faf24dd53
ninhalp <anapereira.ufrj@hotmail.com>
2019-01-30 10:45:44 -0200

    search4vowels 2.0

--- search4vowels.py
+++ search4vowels.py
@@ -4,9 +4,6 @@ def search4vowels(phrase):
   found = []
   for letter in phrase:
     if letter in vowels:
-      if letter not in found:
-        found.append(letter)
-  for vowel in found:
-    print(vowel)
+      found.append(letter)
   counter = Counter(found)
   return counter.most_common(5)
